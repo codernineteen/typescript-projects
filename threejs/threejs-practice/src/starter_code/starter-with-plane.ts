ae87682892dacd957a622b3bc23fd2c51cde3ae0
codernineteen <wjsdPcks34@gmail.com>
2022-09-29 19:34:12 +0900

    Mini projects to practice three.js

---
+++ threejs/threejs-practice/src/starter_code/starter-with-plane.ts
@@ -0,0 +1,65 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { camera, scene, renderer } from "../module/renderer";
+import * as dat from "dat.gui";
+import "./style.css";
+
+const control = new OrbitControls(camera, renderer.domElement);
+control.enableDamping = true;
+const gui = new dat.GUI();
+
+{
+  const light = new THREE.DirectionalLight("white", 0.5);
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+
+  light.position.set(4.3, 7, 7);
+  gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
+  gui.add(light, "intensity", 0, 1, 0.01);
+  gui.add(light.position, "x", -7, 7, 0.1);
+  gui.add(light.position, "y", -7, 7, 0.1);
+  gui.add(light.position, "z", -7, 7, 0.1);
+  scene.add(light, ambientLight);
+}
+
+{
+  const geo = new THREE.PlaneGeometry(40, 40);
+  const mat = new THREE.MeshStandardMaterial({
+    color: "white",
+  });
+  const plane = new THREE.Mesh(geo, mat);
+  plane.rotation.x = Math.PI * -0.5;
+  plane.position.y = -3;
+  scene.add(plane);
+}
+
+{
+  const geo = new THREE.SphereGeometry(1, 20, 20);
+  const mat = new THREE.MeshStandardMaterial({
+    color: "white",
+  });
+  const sphere = new THREE.Mesh(geo, mat);
+  scene.add(sphere);
+}
+
+window.addEventListener("resize", () => {
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+
+  //update camera
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+
+  //update renderer
+  renderer.setSize(width, height);
+  //remove blurry on mesh by modifying pixel ratio
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+camera.position.z += 5;
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  control.update();
+  renderer.render(scene, camera);
+};
+animate();
